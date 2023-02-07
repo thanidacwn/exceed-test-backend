33cc92a4929796d5312bce5ae5a47caca48776cc
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-02-07 14:34:14 +0700

    something

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 from fastapi import FastAPI
-from pymongo import MongoClient
-from dotenv import load_dotenv
 from routers import locker
-import os
-
-load_dotenv(".env")
-username = os.getenv("username")
-password = os.getenv("password")
-
-client = MongoClient(
-    f"mongodb://{username}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT"
-)
-
-db = client["exceed08"]
-collection = db["locker_management_collection"]
 
 app = FastAPI()
 app.include_router(locker.router)
--- routers/locker.py
+++ routers/locker.py
@@ -1,9 +1,33 @@
 from fastapi import APIRouter, Body
-from typing import Union, Optional
+from typing import Union, Optional, List
 from pydantic import BaseModel
+from datetime import timedelta, datetime
+from database.mongo_connection import *
 
 router = APIRouter(
     prefix="/locker",
     tags=["locker"],
     responses={404: {"description": "Not found"}},
 )
+
+class User(BaseModel):
+    user_id: str
+    item: List[str]
+    duration: timedelta
+    start_time: datetime
+    end_time: datetime
+
+class Locker(BaseModel):
+    locker_num: str
+    is_available: bool
+    user: Union[User, None]
+
+
+@router.get("/available_lockers")
+def find_available_locker()->List:
+    """ For checking available lockers."""
+    data = collection.find({"is_available": True})
+    for i in data:
+        print(i)
+
+
